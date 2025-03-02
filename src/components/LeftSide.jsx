ba043e864f777feabaf677860a2d513f96f6e6be
aniketnayek07 <aniketnayek575@gmail.com>
2025-03-02 11:08:28 +0530

    Changes

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import SignUp from "../image/SignUp.png"
 const LeftSide = () => {
   return (
-    <div>LeftSide</div>
-  )
+    <div className="w-1/2 h-full">
+      <div className="w-full h-full bg-[#8E7ED7] flex justify-center align-middle items-center ">
+          <img src={SignUp} alt="SignIn" />      
+      </div>
+    </div>
+  );
 }
 
 export default LeftSide
